2b2d5ffdfc846c6d2ecd44c08ae024a46b1fe7cd
ysh <happysh_s2@naver.com>
2022-01-29 23:19:12 +0900

    Implement Queue

--- Baekjoon/Queue/[baekjoon]18258.playground/Contents.swift
+++ Baekjoon/Queue/[baekjoon]18258.playground/Contents.swift
@@ -1,3 +1,36 @@
+/**
+ *문제 18258
+ *https://www.acmicpc.net/problem/18258
+ */
 import UIKit
-
-var greeting = "Hello, playground"
+let input = readLine()!
+public class Queue<T>{
+    private var data = [T]()
+    public init(){}
+    //push
+    public func enqueue(element: T){
+        data.append(element)
+    }
+    //pop
+    public func dequeue(element: T){
+        data.removeFirst()
+    }
+    public var size :Int {
+        return data.count
+    }
+    public func isEmpty() -> Bool{
+        return data.count >= 1 ? false : true
+    }
+    public func front(){
+        guard let element = data.first else{
+            return print("-1")
+        }
+        print("\(element)")
+    }
+    public func back() -> T{
+        guard let element = data.last else{
+            return print("-1")
+        }
+        print("\(element)")
+    }
+}

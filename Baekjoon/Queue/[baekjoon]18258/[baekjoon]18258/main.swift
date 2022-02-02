b305349ddd62fbdaaa05027ac314bc1502851f60
ysh <happysh_s2@naver.com>
2022-02-02 19:01:40 +0900

    readLine() version. still timeout...

--- Baekjoon/Queue/[baekjoon]18258/[baekjoon]18258/main.swift
+++ Baekjoon/Queue/[baekjoon]18258/[baekjoon]18258/main.swift
@@ -3,48 +3,6 @@
 //  https://www.acmicpc.net/problem/18258
 
 import Foundation
-final class FileIO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-    }
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-        return buffer[index]
-    }
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-        while now == 10
-                || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-        return sum * (isPositive ? 1:-1)
-    }
-    @inline(__always) func readString() -> String {
-        var now = read()
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
-    }
-    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
-        var now = read()
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-        return Array(buffer[beginIndex..<(index-1)])
-    }
-}
 class Queue{
     private var frontQ = [Int]()
     private var rearQ = [Int]()
@@ -82,21 +40,21 @@ class Queue{
 }
 var result = ""
 var queue = Queue();
-var fIO = FileIO();
-for _ in 0 ..< fIO.readInt() {
-    var input = fIO.readString()
-    if input == "push"{
-        queue.enqueue(fIO.readInt())
-    }else if input == "front" {
+for _ in 0 ..< Int(readLine()!)! {
+    var str = readLine()!
+    if str == "front" {
         result += "\(queue.front)" + "\n"
-    }else if input == "pop"{
+    }else if str == "pop"{
         result += "\(queue.dequeue())" + "\n"
-    }else if input == "empty"{
+    }else if str == "empty"{
         result += queue.isEmpty ? "1\n" : "0\n"
-    }else if input == "size"{
+    }else if str == "size"{
         result += "\(queue.size)" + "\n"
-    }else if input == "back"{
+    }else if str == "back"{
         result += "\(queue.back)" + "\n"
+    }else{
+        var str = str.split(separator: " ")
+        queue.enqueue(Int(str[1])!)
     }
 }
 print(result)

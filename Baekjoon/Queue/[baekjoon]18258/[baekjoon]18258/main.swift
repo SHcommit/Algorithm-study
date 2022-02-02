fc942d45db8e1c12403db26e24ca8ab9b5d28e28
ysh <happysh_s2@naver.com>
2022-02-02 19:49:25 +0900

    Fix fast FileIO class

--- Baekjoon/Queue/[baekjoon]18258/[baekjoon]18258/main.swift
+++ Baekjoon/Queue/[baekjoon]18258/[baekjoon]18258/main.swift
@@ -4,45 +4,45 @@
 
 import Foundation
 final class FileIO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
+    private var buffer:[UInt8]
+    private var index: Int
     init(fileHandle: FileHandle = FileHandle.standardInput) {
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
     }
     @inline(__always) private func read() -> UInt8 {
         defer { index += 1 }
-        return buffer[index]
+        return buffer.withUnsafeBufferPointer { $0[index] }
+        
     }
     @inline(__always) func readInt() -> Int {
         var sum = 0
         var now = read()
         var isPositive = true
-        while now == 10
-                || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now == 10 || now == 32 {
+            now = read()
+        } // 공백과 줄바꿈 무시
+        if now == 45{
+            isPositive.toggle();now = read()
+        } // 음수 처리
         while now >= 48, now <= 57 {
             sum = sum * 10 + Int(now-48)
             now = read()
         }
         return sum * (isPositive ? 1:-1)
     }
-    @inline(__always) func readString() -> String {
+    @inline(__always) func readString() -> Int {
+        var str = 0
         var now = read()
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
-    }
-    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
-        var now = read()
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-        return Array(buffer[beginIndex..<(index-1)])
+        while now == 10 || now == 32 {
+            now = read()
+        } // 공백과 줄바꿈 무시
+        while now != 10 && now != 32 && now != 0 {
+            str += Int(now)
+            now = read()
+            
+        }
+        return str
     }
 }
 class Queue{
@@ -84,18 +84,18 @@ var result = ""
 var queue = Queue();
 var fIO = FileIO();
 for _ in 0 ..< fIO.readInt() {
-    var input = fIO.readString()
-    if input == "push"{
+    let input = fIO.readString()
+    if input == 448{
         queue.enqueue(fIO.readInt())
-    }else if input == "front" {
+    }else if input == 553 {
         result += "\(queue.front)" + "\n"
-    }else if input == "pop"{
+    }else if input == 335{
         result += "\(queue.dequeue())" + "\n"
-    }else if input == "empty"{
+    }else if input == 559{
         result += queue.isEmpty ? "1\n" : "0\n"
-    }else if input == "size"{
+    }else if input == 443{
         result += "\(queue.size)" + "\n"
-    }else if input == "back"{
+    }else if input == 401{
         result += "\(queue.back)" + "\n"
     }
 }

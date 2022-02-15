f89d864df6d699de1503492f70c2d6e59bfe47ac
ysh <happysh_s2@naver.com>
2022-02-15 17:57:55 +0900

    Implement code. But I was wrong..

--- Baekjoon/Queue/[baekjoon]11866/[baekjoon]11866/main.swift
+++ Baekjoon/Queue/[baekjoon]11866/[baekjoon]11866/main.swift
@@ -6,6 +6,76 @@
 //
 
 import Foundation
+/**
+class Queue {
+    var queue : Array<Int>
+    init(_ element : Int){
+        queue = Array(1...element)
+    }
+    func enQueue(_ element : Int){
+        queue.append(element);
+    }
+    func deQueue(_ index: Int) -> Int {
+        return queue.remove(at: index)
+    }
+    var size : Int {
+        queue.count
+    }
+}*/
 
-print("Hello, World!")
-
+let input = readLine()!.split(separator: " ")
+var N = Int(input[0])!
+var array :Array<Int> = Array(1...N);
+var cnt = 0; //몇번 반복하는가. -> N 번만큼.
+let std = Int(input[1])! //std 만큼 나누어야함
+var index = 0;
+//var q = Queue(N)
+if( N >= std){
+var result : String = "<"
+index = index + std - 1
+cnt += 1
+result += "\(array[index]), "
+array[index] = -1
+while(cnt != N ){
+    //index += std
+    for _ in 1...std{
+        index += 1
+        if index >= N {
+            index %= N
+        }
+        if array[index] == -1{
+            while array[index] == -1 {
+                index += 1
+                if index >= N {
+                    index %= N
+                }
+            }
+        }
+    }
+    result += "\(array[index])"
+    cnt += 1
+    if cnt == 7 {
+        break;
+    }
+    result += ", "
+    array[index] = -1;
+}
+print(result + ">")
+}
+else{
+    NSLog("error")
+}
+//var index = 0; //index == 0~6까지, 근데 count = 3일 때 index는 2니까 index +1 해야함.
+/*while(cnt != N){
+    if index > N {
+        index = index % N;
+    }
+    if (index + 1) % count == 0 {
+        result += "\(q.deQueue(index)), "
+        index += 1
+    }else{
+        
+    }
+}
+*/
+//print(result + ">")

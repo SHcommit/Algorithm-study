b9d0f42b1156a0528f9c2b4ef6b6341cd4dff061
ysh <happysh_s2@naver.com>
2022-02-19 00:01:17 +0900

    Implement insertionSort.

--- Baekjoon/Sort/[baekjoon]2750/[baekjoon]2750/main.swift
+++ Baekjoon/Sort/[baekjoon]2750/[baekjoon]2750/main.swift
@@ -4,3 +4,29 @@
 
 import Foundation
 
+func insertionSort(_ array: inout Array<Int>){
+    if array.count <= 1 {
+        return;
+    }
+    for i in 1..<array.count{
+        var x = array[i];
+        var j = i;
+        while j > 0 && array[j-1] > x {
+            array[j] = array[j - 1]
+            j -= 1;
+        }
+        array[j] = x
+    }
+}
+
+let N = Int(readLine()!)!
+var array = Array<Int>()
+for _ in 0..<N{
+    var input = Int(readLine()!)!
+    array.append(input)
+}
+
+insertionSort(&array);
+for i in 0..<array.count{
+    print("\(array[i])")
+}

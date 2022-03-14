a5300cf4a70444b7e86ad167b75d4c1fb281f99c
ysh <happysh_s2@naver.com>
2022-03-15 03:06:26 +0900

    Add sum

--- Baekjoon/Normal/[baekjoon]2506/[baekjoon]2506/main.swift
+++ Baekjoon/Normal/[baekjoon]2506/[baekjoon]2506/main.swift
@@ -1,11 +1,12 @@
-//
 //  main.swift
 //  [baekjoon]2506
-//
 //  Created by 양승현 on 2022/03/15.
-//
-
 import Foundation
-
-print("Hello, World!")
-
+var sum = 0
+var seq = 0
+var n = Int(readLine()!)!
+var array = readLine()!.split(separator: " ").map{Int($0)!}
+for i in 0..<array.count{
+    array[i] == 0 ? seq = 0 : {seq += 1; sum += seq}()
+}
+print("\(sum)")

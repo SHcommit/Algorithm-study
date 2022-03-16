e8403dbeedd197842601097c15356542c6db646f
ysh <happysh_s2@naver.com>
2022-03-16 22:46:03 +0900

    Update file

--- Baekjoon/DFS&BFS/[backjoon]2606/[backjoon]2606/main.swift
+++ Baekjoon/DFS&BFS/[backjoon]2606/[backjoon]2606/main.swift
@@ -15,16 +15,7 @@
  4 7
  */
 import Foundation
-var img = """
-\\    /\
- )  ( ')
-(  /  )
- \\\(\\_\\_)|
 
-"""
-
-print("\(img")
-/*
 var vertex = Int(readLine()!)!
 var n = Int(readLine()!)!
 var cnt = 0
@@ -36,37 +27,22 @@ for i in 0..<n{
     graph[input[1]].append(input[0])
 }
 
-for i in 0..<graph.count{
-    graph[i].sort(by: ){ $0 > $1 }
-}
 
 func dfs (startVertex s : Int) {
-    
     var visited = [Int]()
     var stack = [Int]()
     stack.append(s)
     while(!stack.isEmpty){
-        /*
-         stack에 일단
-         1일 때 2탐색해야함
-         2는 3
-         3은없으니까 pop
-         
-         2는 5랑 연결됨
-         5는 없으니까 pop
-         **/
-        var node = stack.last!
-    
+        var node = stack.popLast()!
         if(!visited.contains(node)){
+            
             visited.append(node)
-            cnt = cnt + 1
-            stack.append(graph[node].last!)
-        }else{
-            stack.popLast()
+            if visited.count > 1 {
+                cnt = cnt + 1
+            }
+            stack.append(contentsOf: graph[node].sorted(by: >))
         }
     }
 }
 dfs(startVertex: 1)
-
 print("\(cnt)")
-*/
---
+++ Baekjoon/DFS&BFS/[baekjoon]1260RE/[baekjoon]1260RE/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  [baekjoon]1260RE
+//
+//  Created by 양승현 on 2022/03/16.
+//
+
+import Foundation
+
+print("Hello, World!")
+

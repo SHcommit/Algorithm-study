b9bee1857c7066be11b471a9b4ad28939dab5dd4
ysh <happysh_s2@naver.com>
2022-03-17 00:42:33 +0900

    delete file

--- Baekjoon/DFS&BFS/[baekjoon]2178/[baekjoon]2178/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]2178/[baekjoon]2178/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
-
+var nm = readLine()!.split(separator: " ").map{Int($0)!}
+var map : [[Int]] = Array(repeating: Array(repeating: 0, count: 0), count: nm[0]+1)
+for i in 0..<nm[0]{
+    map[i].append(contentsOf: {
+        readLine()!.split(separator: " ").map{Int($0)!}
+    }())
+}
+func bfs (start vertex : Int){
+    var queue = [Int]()
+    var visited = [Int]()
+    var cnt = 1
+    var x = 0
+    var y = 0
+    
+    while(x != nm[0]-1 && y != nm[1]){
+        
+        if map[y+1][x] == 1 {
+            
+        }
+        if map[y][x+1] == 1 {
+            
+        }
+        
+    }
+}

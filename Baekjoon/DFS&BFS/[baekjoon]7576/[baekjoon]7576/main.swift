b80e2c5a45a9dbcaa4c09fbcd347ce56abbd4584
ysh <happysh_s2@naver.com>
2022-03-24 22:21:24 +0900

    Create file

--- Baekjoon/DFS&BFS/[baekjoon]7576/[baekjoon]7576/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]7576/[baekjoon]7576/main.swift
@@ -1,11 +1,9 @@
-//
-//  main.swift
 //  [baekjoon]7576
-//
 //  Created by 양승현 on 2022/03/21.
-//
 
 import Foundation
 
-print("Hello, World!")
+var MN = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+var graph = Array(repeating: Array(repeating: 0, count: MN[0]), count: MN[1])
 
---
+++ Baekjoon/Stack/[baekjoon]10828/[baekjoon]10828/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  [baekjoon]10828
+//
+//  Created by 양승현 on 2022/03/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+

2c9862f8e5b723106dd0dddc9f8fec12e92d1e15
ysh <happysh_s2@naver.com>
2022-03-26 00:26:30 +0900

    Create [baekjoon]24039

---
+++ Baekjoon/Normal/[baekjoon]24039/[baekjoon]24039/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  [baekjoon]24039
+//
+//  Created by 양승현 on 2022/03/26.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- Baekjoon/Stack/[baekjoon]10828/[baekjoon]10828/main.swift
+++ Baekjoon/Stack/[baekjoon]10828/[baekjoon]10828/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+var n = Int(readLine()!)!
+var stack = [String]()
+var res = ""
+for _ in 0..<n {
+    var command = readLine()!.split(separator: " ").map{String($0)}
+    switch command[0]{
+    case "push":
+        stack.append(command.last!)
+    case "pop":
+        let pop = stack.popLast() ?? "-1\n"
+        res += pop+"\n"
+    case "top":
+        let top  = stack.last ?? "-1\n"
+        res += top+"\n"
+    case "empty":
+        res = stack.isEmpty ? res + "1\n" : res + "0\n"
+    case "size":
+        res += "\(stack.count)\n"
+    default:
+        break;
+    }
+}
 
+print(res)

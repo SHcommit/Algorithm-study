bca38df006ec8e5043b35103f25711aab00e3049
ysh <happysh_s2@naver.com>
2022-03-30 23:02:19 +0900

    Update proj

--- Baekjoon/백트래킹/[baekjoon]15649/[baekjoon]15649/main.swift
+++ Baekjoon/백트래킹/[baekjoon]15649/[baekjoon]15649/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+//input[0] == n
+//input[1] == m
+var input = readLine()!.split(separator: " ").map{Int(String($0))!}
 
+
+var visit = Array(repeating: false, count: 9)
+var stack = [Int]()
+
+var res  = ""
+
+func dfs(index :Int){
+    if index > input[1]{
+        //seq.reversed()
+        stack.forEach{
+            res += "\($0) "
+        }
+        res += "\n"
+        return;
+    }
+
+    for i in 1...input[0]{
+        if(!visit[i]){
+            visit[i] = true;
+            stack.append(i);
+            dfs(index: index + 1)
+            stack.popLast()
+            visit[i] = false;
+        }
+    }
+}
+
+dfs(index: 1)
+print(res);

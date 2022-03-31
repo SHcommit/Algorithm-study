0a9a38c9a821212451bba5ad5786d3c21a945d19
ysh <happysh_s2@naver.com>
2022-03-31 22:25:08 +0900

    Implement

--- Baekjoon/DFS&BFS/[baekjoon]11724/[baekjoon]11724/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]11724/[baekjoon]11724/main.swift
@@ -6,43 +6,37 @@ import Foundation
 
 var connected = 0;
 
-func solution(){
-    var NM = readLine()!.split(separator: " ").map{Int(String($0))!}
-    if NM[1] == 0{
-        connected = 1
-        return
-    }
-    //편의상 1부터 해야지,, 정점이 1부터 시작이니까
-    var graph = Array(repeating: Array(repeating: 0, count: NM[0] - 1), count: NM[0]+1)
-    var visited = Array(repeating: false, count: NM[0]+1)
+var NM = readLine()!.split(separator: " ").map{Int(String($0))!}
+//편의상 1부터 해야지,, 정점이 1부터 시작이니까
+var graph = [Int:[Int]]()
+
+var visited = Array(repeating: false, count: NM[0] + 1)
     
-    for i in 0..<NM[1]{
-        var UV = readLine()!.split(separator: " ").map{Int(String($0))!}
-        graph[UV[0]].append(UV[1])
-        graph[UV[1]].append(UV[0])
+for i in 1...NM[0]{
+    graph.updateValue([], forKey: i);
+}
+for i in 0..<NM[1]{
+    var UV = readLine()!.split(separator: " ").map{Int(String($0))!}
+    graph[UV[0]]?.append(UV[1])
+    graph[UV[1]]?.append(UV[0])
+}
+for i in 1...NM[0]{
+    if visited[i]{
+        continue
     }
-    
-    if  NM[1] == 1{
-        connected = 1;
-        return;
-    }
-    for i in 1..<NM[0]{
-        var stack = [Int]()
-        if visited[i] == true{
-            continue
+    dfs(vertex : i)
+    connected += 1
+}
+        
+func dfs(vertex v :Int){
+    var stack = [Int]()
+    stack.append(v)
+    while(!stack.isEmpty){
+        let vertex = stack.removeLast()
+        if(!visited[vertex]){
+            visited[vertex] = true
+            stack.append(contentsOf: graph[vertex]!.sorted(by: >))
         }
-    
-        stack.append(i);
-        while(!stack.isEmpty){
-            var vertax = stack.popLast()!
-            if !visited[vertax] {
-                visited[vertax] = true;
-                stack.append(contentsOf: graph[vertax].filter{$0 > 0 && visited[$0] == false})
-            }
-        }
-    
-        connected += 1
     }
 }
-solution();
 print(connected);
--- Baekjoon/Normal/[baekjoon]10816/[baekjoon]10816/main.swift
+++ Baekjoon/Normal/[baekjoon]10816/[baekjoon]10816/main.swift
@@ -1,11 +1,24 @@
-//
 //  main.swift
 //  [baekjoon]10816
-//
 //  Created by 양승현 on 2022/03/31.
-//
 
 import Foundation
 
-print("Hello, World!")
+var N = Int(readLine()!)!
+var SGsCard = readLine()!.split(separator: " ").map{Int(String($0))!}
+var M = Int(readLine()!)!
+var givenNum = readLine()!.split(separator: " ").map{Int(String($0))!}
+var dic = [Int:Int]()
+var res = ""
 
+for i in SGsCard.sorted() {
+    dic[i] = dic.keys.contains(i) ? dic[i]! + 1 : 1
+}
+for i in givenNum {
+    if dic.keys.contains(i){
+        res += "\(dic[i]!) "
+    }else{
+        res += "\(0) "
+    }
+}
+print(res)

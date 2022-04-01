ce42af191d517d3804fc1020345ab91c8419bc3c
ysh <happysh_s2@naver.com>
2022-04-01 16:59:15 +0900

    Add code review

---
+++ Baekjoon/DFS&BFS/[baekjoon]2468/[baekjoon]2468/main.swift
@@ -0,0 +1,65 @@
+//  main.swift
+//  [baekjoon]2468
+//  Created by 양승현 on 2022/04/01.
+/**
+ * [문제 : 2468](https://www.acmicpc.net/problem/2468)
+ * 간단한 코드리뷰.
+ *
+ * 안전가옥 == land로 지정하고, 값을 받을 때, 최소 h랑 최대 h를 구했습니다.
+ * 그리고 비가 내릴 수 있는 최소 h 부터 최대h 까지. 순차적으로 반복하면서, 특정 h에 안전가옥의 크기를 구한 후
+ * 안전가옥 최대인지를 구했습니다.
+ */
+import Foundation
+
+var h = Int(readLine()!)!
+var safety = 1;
+
+var land = Array(repeating: [Int](), count: h)
+var minH = 3
+var maxH = 1
+var direction = [(-1,0),(1,0),(0,-1),(0,1)]
+
+for i in 0..<h{
+    land[i] = readLine()!.split(separator: " ").map{
+        if Int(String($0))! < minH{
+            minH = Int(String($0))!
+        }
+        if Int(String($0))! > maxH{
+            maxH = Int(String($0))!
+        }
+        return Int(String($0))!
+    }
+}
+
+for i in minH...maxH{
+    var count = 0;
+    var visited = Array(repeating: Array(repeating: false, count: h), count: h)
+    
+    for y in 0..<h{
+        for x in 0..<h{
+            if !visited[y][x] && land[y][x] > i{
+                //visited[y][x] = true
+                dfs(x,y,i, &visited)
+                count += 1
+            }
+        }
+    }
+    if safety < count {
+        safety = count;
+    }
+}
+func dfs(_ x :Int, _ y :Int, _ height:Int, _ visited : inout [[Bool]]){
+    for i in direction {
+        let nx = x+i.0
+        let ny = y+i.1
+        if ny < 0 || ny > h - 1 || nx < 0 || nx > h - 1 {
+            continue
+        }
+        if !visited[ny][nx] && land[y][x] > height {
+            visited[ny][nx] = true;
+            dfs(nx,ny,height, &visited)
+        }
+    }
+}
+
+print(safety)

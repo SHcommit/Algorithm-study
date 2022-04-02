625df3ccb54b5d86edc2b6a7836d21e9efd7e1f3
ysh <happysh_s2@naver.com>
2022-04-02 15:30:03 +0900

    Add func

--- Baekjoon/Queue/[baekjoon]10845/[baekjoon]10845/main.swift
+++ Baekjoon/Queue/[baekjoon]10845/[baekjoon]10845/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+var queue = [String]()
 
+for _ in 0..<Int(readLine()!)! {
+    let input = readLine()!.components(separatedBy: " ")
+
+    switch input[0] {
+    case "push":
+        queue.append(input[1])
+    case "pop":
+        if queue.isEmpty {
+            print(-1)
+        } else {
+            print(queue[0])
+            queue.removeFirst()
+        }
+    case "size":
+        print(queue.count)
+    case "empty":
+        print(queue.isEmpty ? 1 : 0)
+    case "front":
+        print(queue.first ?? -1)
+    case "back":
+        print(queue.last ?? -1)
+    default:
+        break
+    }
+}

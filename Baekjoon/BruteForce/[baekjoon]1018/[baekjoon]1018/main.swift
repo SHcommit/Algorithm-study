f7bd93fc5ec558cf79ee236b739ff72f34e29653
ysh <happysh_S2@naver.com>
2022-04-30 19:22:12 +0900

    Add code review

--- Baekjoon/BruteForce/[baekjoon]1018/[baekjoon]1018/main.swift
+++ Baekjoon/BruteForce/[baekjoon]1018/[baekjoon]1018/main.swift
@@ -7,22 +7,18 @@
 
 import Foundation
 
-var model : Model
-
-let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
-
-model = Model(width: nm[1], height: nm[0])
-
-bruteForce()
-
-print(model.count)
+if let model = inputData(){
+    bruteForce(model: model)
+    print(model.count)
+}
 
 
-func bruteForce(){
+// M x N을 탐색하는 보드의 처음과 끝이 8보다 작다면 다음 줄을 순회하도록 했습니다.
+func bruteForce(model : Model){
     for y in 0..<model.height{
         for x in 0..<model.width{
             if model.width - x >= model.minLine && model.height - y >= model.minLine{
-                let tmp = drawBoard(x: x, y: y)
+                let tmp = drawBoard(x: x, y: y, model: model)
                 if tmp < model.count{
                     model.count = tmp
                 }
@@ -32,7 +28,23 @@ func bruteForce(){
         }
     }
 }
-func drawBoard(x :Int, y: Int) -> Int{
+
+// 데이터를 입력받습니다.
+func inputData() -> Model?{
+    if let input = readLine(){
+        let nm = input.split(separator: " ").map{Int(String($0))!}
+        return Model(width: nm[1], height: nm[0])
+    }
+    return nil
+}
+
+/**
+ * 보드의 최소 draw를 탐색하는 함수입니다.
+ * 보드의 시작점이 "W" 일때 or "B" 일 때 두 가지의 경우로 나누어
+ * W == 0일때 칠해야 할 최소 경우를 aCnt
+ * B == 0일 때 칠해야 할 최소 경우를 bCnt로 둔 후 최소값을 반환했습니다.
+ */
+func drawBoard(x :Int, y: Int, model: Model) -> Int{
     var aCnt = 0
     var bCnt = 0
     for dy in 0..<model.minLine{
@@ -55,6 +67,7 @@ func drawBoard(x :Int, y: Int) -> Int{
 }
 
 
+// 자료형들입니다.
 class Model {
     
     var width : Int
@@ -70,8 +83,9 @@ class Model {
         count = 9999
         board = Array(repeating: [String](), count: height)
         for i in 0..<height{
-            board[i] = readLine()!.map{String($0)}
+            if let line = readLine(){
+                board[i] = line.map{String($0)}
+            }
         }
     }
 }
-

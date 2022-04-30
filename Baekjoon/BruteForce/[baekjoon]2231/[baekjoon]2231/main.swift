b5d5adbcacfe14a03bb7a4db4ec179cd6fd6a205
ysh <happysh_S2@naver.com>
2022-04-30 22:53:28 +0900

    Solve the problem

--- Baekjoon/BruteForce/[baekjoon]2231/[baekjoon]2231/main.swift
+++ Baekjoon/BruteForce/[baekjoon]2231/[baekjoon]2231/main.swift
@@ -6,6 +6,28 @@
 //
 
 import Foundation
+/**
+ * 분해합 : 
+ */
+var n = Int(readLine()!)!
+var result = 1000000
 
-print("Hello, World!")
+for i in 1..<n {
+    
+    var temp = i
+    var sum = 0
+    
+    while(temp != 0){
+        sum += temp % 10
+        temp = temp / 10
+        if temp == 0 {
+            sum += i
+            
+            if result > sum  && n == sum {
+                result = i
+            }
+        }
+    }
+}
 
+print(result == 1000000 ? "0" : "\(result)")

c6054d5a3709e4a7d5b82b95ed12870a6104dfc8
ysh <happysh_S2@naver.com>
2022-05-09 11:06:21 +0900

    Add code review`

--- Baekjoon/BinarySearch/[baekjoon]11561/[baekjoon]11561/main.swift
+++ Baekjoon/BinarySearch/[baekjoon]11561/[baekjoon]11561/main.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+//이상하게 right c++에서도 right 를 (long long)le9 해야 답이되는지 모르겠네,,
+
+// 어느 mid값 개수에 따라서 N에 도착하는지를 구하는 것이 문제입니다.
+// 이때 특정 점프 조건이 갖추어진다면, 이분 탐색을 통해 점프가 되는 첫번째 구간을 탐색할 수 있습니다.
+
 var n = Int(readLine()!)!
 var res = ""
 func binary_search( lastBridgeNumber end : Int, answer ans : inout Int){
@@ -19,6 +24,7 @@ func binary_search( lastBridgeNumber end : Int, answer ans : inout Int){
     print(ans)
     
 }
+
 func isArrive(maxJumpCount jumpCnt: Int, BridgeNumber end: Int)->Bool{
     if jumpCnt*(jumpCnt+1) / 2 <= end {
         return true

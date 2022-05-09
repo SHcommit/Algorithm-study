15bd74e25fd1f6d869f165f1b5b27b1689f430d0
ysh <happysh_S2@naver.com>
2022-05-09 11:00:39 +0900

    Solve the problem

--- Baekjoon/BinarySearch/[baekjoon]11561/[baekjoon]11561/main.swift
+++ Baekjoon/BinarySearch/[baekjoon]11561/[baekjoon]11561/main.swift
@@ -1,11 +1,35 @@
-//
-//  main.swift
-//  [baekjoon]11561
-//
-//  Created by 양승현 on 2022/05/09.
-//
-
 import Foundation
 
-print("Hello, World!")
+var n = Int(readLine()!)!
+var res = ""
+func binary_search( lastBridgeNumber end : Int, answer ans : inout Int){
+    var left = 1
+    var right = 1000000000
+    var mid : Int
+    while(left <= right){
+        mid = (left + right) / 2
+        if isArrive(maxJumpCount: mid, BridgeNumber: end){
+            ans = mid
+            left = mid + 1
+        }else{
+            right = mid - 1
+            
+        }
+    }
+    print(ans)
+    
+}
+func isArrive(maxJumpCount jumpCnt: Int, BridgeNumber end: Int)->Bool{
+    if jumpCnt*(jumpCnt+1) / 2 <= end {
+        return true
+    }else{
+        return false
+    }
+}
 
+for i in 0..<n{
+    var answer = 0
+    var bridgeNumber = Int(readLine()!)!
+    binary_search(lastBridgeNumber: bridgeNumber, answer: &answer)
+}
+print(res)

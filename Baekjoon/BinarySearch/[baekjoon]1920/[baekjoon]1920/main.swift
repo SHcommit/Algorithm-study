a02fcb6d901626f026c72e5bfbe374b2f362511b
Yangseunghyun <happysh_s2@naver.com>
2022-05-11 01:11:15 +0900

    Solve the problem

--- Baekjoon/BinarySearch/[baekjoon]1920/[baekjoon]1920/main.swift
+++ Baekjoon/BinarySearch/[baekjoon]1920/[baekjoon]1920/main.swift
@@ -1,11 +1,23 @@
-//
-//  main.swift
-//  [baekjoon]1920
-//
-//  Created by 양승현 on 2022/05/10.
-//
-
 import Foundation
-
-print("Hello, World!")
-
+var n    = Int(readLine()!)!
+var list = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted(by: <)
+var secN = Int(readLine()!)!
+var isExistNum : [Int] = readLine()!.split(separator: " ").map{Int($0)!}
+func binary_search(arr: [Int], target: Int) -> Int{
+    var left = 0
+    var right = arr.count - 1
+    while left <= right {
+        let mid = (left + right) / 2
+            if arr[mid] == target {
+                return 1
+            }else if arr[mid] > target {
+                right = mid - 1
+            }else if arr[mid] < target {
+                left = mid + 1
+            }
+        }
+        return 0
+}
+for i in 0..<secN {
+    print(binary_search(arr: list, target: isExistNum[i]))
+}

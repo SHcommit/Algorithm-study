32edf2b11ddea6b9edc29afe10b4fe34d044bcb1
Yangseunghyun <happysh_s2@naver.com>
2022-05-12 01:14:25 +0900

    Solve the problem

--- Baekjoon/BinarySearch/[baekjoon]10815/[baekjoon]10815/main.swift
+++ Baekjoon/BinarySearch/[baekjoon]10815/[baekjoon]10815/main.swift
@@ -1,11 +1,32 @@
-//
-//  main.swift
-//  [baekjoon]10815
-//
-//  Created by 양승현 on 2022/05/12.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+func BOJ_10815(){
+    var n      = Int(readLine()!)!
+    var myCard = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted(by: <)
+    var m      = Int(readLine()!)!
+    var list   = readLine()!.split(separator: " ").map{Int(String($0))!}
+    var res    = ""
+    
+    for i in 0..<list.count{
+        binary_search(FirstList: myCard, target: list[i], result: &res)
+    }
+    print(res)
+}
+func binary_search(FirstList myCard : [Int], target : Int, result res : inout String){
+    var left  = 0
+    var right = myCard.count - 1
+    while(left <= right){
+        let mid = (left + right) / 2
+        if myCard[mid] == target {
+            res += "\(1) "
+            return
+        }else if myCard[mid] < target {
+            left = mid + 1
+        }
+        else if myCard[mid] > target{
+            right = mid - 1
+        }
+    }
+    res += "\(0) "
+}
+BOJ_10815()

44ca4f6cfe1f37740e273f672811254f8dd37081
YangSeungHyun <happysh_s2@naver.com>
2022-05-13 18:11:52 +0900

    Solve the problem

--- Baekjoon/DFS&BFS/[baekjoon]7569/[baekjoon]7569/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]7569/[baekjoon]7569/main.swift
@@ -1,22 +1,5 @@
 import Foundation
-/**
- * 토마토 보관중  M x N(가 x 세) x h
- * 이번엔 높이까지 추가됨
- *
- * 창고에는 잘 익은것 익지않은 것 두 가지 종류가 존재
- * 하루가 지나면 익은. 토마토를 기준으로 익지 않은 토마토들이 익게됨
- * 이때 상 하 좌 우 + 위 아래까지
- *      이때 대각선으로 익는 토마토는 없고 혼자 익는 토마토도 없음
- *
- * 창고에 보관된 토마토들이 며칠 지났을 경우 다 익는지 알고시퍼
- * 근데 상자에는 일부 토마토가 안들어있을수도있어
- *-----------------------*-----------------------*-----------------------*-----------------------
- *입력
- *MN 과 h ( 2 ≤ M ≤ 100, 2 ≤ N ≤ 100, 1 ≤ H ≤ 100 )
- *두번째줄부터 **가장 밑의 상자**부터 가장 위의 상자까지 저장된 토마토들의 정보 주어짐
- *
- *1은 익은 토마토. 0은 익지 않은 토마토. -1은 토마토 안들어있음
- */
+
 func edgeCheck(nx : Int,ny : Int ,nz :Int,width : Int, height : Int, n : Int) -> Bool{
     if nx < 0 || nx > width - 1 || ny < 0 || ny > height - 1 || nz < 0 || nz > n - 1 {
         return true
@@ -24,13 +7,12 @@ func edgeCheck(nx : Int,ny : Int ,nz :Int,width : Int, height : Int, n : Int) ->
     return false
 }
 
-func bfs(width : Int, height: Int, boxCount zCount : Int,box box : inout [[[Int]]], visit : inout [[[Bool]]], queue : inout [(Int,Int,Int)]){
+func bfs(width : Int, height: Int, boxCount zCount : Int,box : inout [[[Int]]], visit : inout [[[Bool]]], queue : inout [(Int,Int,Int)]){
     let dictionary = [(-1,0,0),(1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
     var index      = 0
     
     while queue.count != index {
         let (curX,curY,curZ) = queue[index]
-//test(map: box, w: width, h: height, c: zCount)
         for (dx,dy,dz) in dictionary{
             let (nx,ny,nz) = (curX + dx, curY + dy, curZ + dz)
             if edgeCheck(nx: nx, ny: ny, nz: nz, width: width, height: height, n: zCount){
@@ -45,6 +27,19 @@ func bfs(width : Int, height: Int, boxCount zCount : Int,box box : inout [[[Int]
         index += 1
     }
 }
+//익은 토마토 큐에 추가
+func ripedTomato(boxCount : Int, height : Int, width : Int, visit : [[[Bool]]], box : [[[Int]]], queue : inout [(Int,Int,Int)]){
+    for z in 0..<boxCount{
+        for y in 0..<height{
+            for x in 0..<width{
+                if visit[z][y][x] == false && box[z][y][x] == 1{
+                    queue.append((x,y,z))
+                }
+            }
+        }
+    }
+}
+
 func BOJ_7569(){
     let WHN    = readLine()!.split(separator: " ").map{Int(String($0))!}
     let width  = WHN[0]
@@ -60,20 +55,11 @@ func BOJ_7569(){
             box[z][y] = readLine()!.split(separator: " ").map{Int(String($0))!}
         }
     }
-//test(map: box, w: width, h: height, c: n)
     //익은 토마토들을 큐에 집어넣음
-    for z in 0..<n{
-        for y in 0..<height{
-            for x in 0..<width{
-                if visit[z][y][x] == false && box[z][y][x] == 1{
-                    queue.append((x,y,z))
-                }
-            }
-        }
-    }
+    ripedTomato(boxCount: n, height: height, width: width, visit: visit, box: box, queue: &queue)
     
     bfs(width: width, height: height, boxCount: n, box: &box, visit: &visit, queue: &queue)
-    
+    //결과 출력
     for z in 0..<n{
         for y in 0..<height{
             for x in 0..<width{
@@ -90,21 +76,5 @@ func BOJ_7569(){
     print(result == 0 ? 1 : result - 1)
     
 }
-/*
-func test(map : [[[Int]]],w : Int,h : Int, c : Int){
-    print()
-    for z in 0..<c{
-        for y in 0..<h{
-            var res = ""
-            for x in 0..<w{
-                res += "\(map[z][y][x]) "
-            }
-            print(res)
-        }
-        print()
-    }
-    print()
-    print()
-}*/
 
 BOJ_7569()

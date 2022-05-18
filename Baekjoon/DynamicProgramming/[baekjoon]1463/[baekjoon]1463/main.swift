cc1146b4ece6b125d3f6c57a0966268dcf821937
ysh <happysh_s2@naver.com>
2022-05-18 20:34:34 +0900

    Solve the problem

--- Baekjoon/DynamicProgramming/[baekjoon]1463/[baekjoon]1463/main.swift
+++ Baekjoon/DynamicProgramming/[baekjoon]1463/[baekjoon]1463/main.swift
@@ -1,11 +1,25 @@
-//
-//  main.swift
-//  [baekjoon]1463
-//
-//  Created by 양승현 on 2022/05/18.
-//
-
 import Foundation
 
-print("Hello, World!")
+func BOJ_1463(){
+    let n = Int(readLine()!)!
+
+    var dp = Array(repeating: 1, count: n+1)
+    
+    dp[1] = 0
+    if n >= 2 {
+        for num in 2...n{
+            dp[num] = 1000000
+            if num % 3 == 0{
+                dp[num] = dp[num/3] + 1 < dp[num] ? dp[num/3] + 1 : dp[num]
+            }
+            if num % 2 == 0{
+                dp[num] = dp[num/2] + 1 < dp[num] ? dp[num/2] + 1 : dp[num]
+            }
+            dp[num] = dp[num - 1] + 1 < dp[num] ? dp[num - 1] + 1 : dp[num]
+        }
+    }
+    print(dp[n])
+}
+
+BOJ_1463()
 

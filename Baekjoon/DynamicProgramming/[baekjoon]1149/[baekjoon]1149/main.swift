c66404941b28b5f4410d429b27f5e4410e448170
ysh <happysh_s2@naver.com>
2022-05-19 21:52:45 +0900

    Update file

---
+++ Baekjoon/DynamicProgramming/[baekjoon]1149/[baekjoon]1149/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  [baekjoon]1149
+//
+//  Created by 양승현 on 2022/05/19.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- Baekjoon/Tree/[baekjoon]11725/[baekjoon]11725/main.swift
+++ Baekjoon/Tree/[baekjoon]11725/[baekjoon]11725/main.swift
@@ -1,11 +1,28 @@
-//
-//  main.swift
-//  [baekjoon]11725
-//
-//  Created by 양승현 on 2022/05/19.
-//
-
 import Foundation
 
-print("Hello, World!")
+func dfs(vertax v : Int, ans : inout [Int], tree : inout [[Int]]){
+    for i in 0..<tree[v].count{
+        if ans[tree[v][i]] == 0{
+            ans[tree[v][i]] = v
+            dfs(vertax: tree[v][i], ans: &ans, tree: &tree)
+        }
+    }
+}
+func BOJ_11725(){
+    let n    = Int(readLine()!)!
+    var tree = Array(repeating: [Int](), count: n + 1)
+    var ans  = Array(repeating: 0, count: n + 1)
+    
+    for _ in 1..<n{
+        let node = readLine()!.split(separator: " ").map{Int(String($0))!}
+        tree[node[0]].append(node[1])
+        tree[node[1]].append(node[0])
+    }
+    dfs(vertax: 1, ans: &ans, tree: &tree)
+    for i in 2...n{
+        if ans[i] == 0{ continue }
+        print(ans[i])
+    }
+}
 
+BOJ_11725()

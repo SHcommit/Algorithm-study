832b568959ab9b08d60d2607f272b0fb76c54787
ysh <happysh_s2@naver.com>
2022-06-29 00:01:33 +0900

    save code

--- Baekjoon/DFS&BFS/[baekjoon]5427/[baekjoon]5427/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]5427/[baekjoon]5427/main.swift
@@ -2,28 +2,13 @@ import Foundation
 
 typealias qElement = (Int,Int,Int)
 
-func isOutEdge(_ nx : Int, _ ny : Int, _ curX : Int, _ curY : Int, _ width: Int, _ height : Int, building : [[String]], isEscape : inout Bool) -> Bool
-{
-    if nx < 0 || nx > width - 1 || ny < 0 || ny > height - 1
-    {
-        if building[curY][curX] == "@"
-        {
-            isEscape = true
-            return false
-        }
-        return true
-    }
-    return false
-}
 func fire(_ width : Int, _ height : Int, _ x: Int, _ y : Int, _ prevTime : Int, _ time : Int, _ visited : inout [[Bool]], _ building : inout [[String]], _ queue : inout[qElement])
 {
     let direction = [(-1,0),(1,0),(0,1),(0,-1)]
-    var temp      = false
-    
     for (dx,dy) in direction
     {
         let (nx, ny) = (dx + x, dy + y)
-        if isOutEdge(nx, ny, x, y, width, height, building: building, isEscape: &temp)
+        if nx < 0 || nx > width - 1 || ny < 0 || ny > height - 1
         {
             continue
         }
@@ -33,10 +18,6 @@ func fire(_ width : Int, _ height : Int, _ x: Int, _ y : Int, _ prevTime : Int, 
             queue.append((nx,ny,prevTime + 1))
             building[ny][nx] = "*"
         }
-//        if visited[ny][nx] == true && building[ny][nx] == "@"
-//        {
-//            building[ny][nx] = "*"
-//        }
     }
 }
 func escape(_ width : Int, _ height : Int, _ time : inout Int, _ res : inout String, _ isEscape : inout Bool, _ visited: inout [[Bool]], _ building : inout [[String]], _ queue : inout [qElement])
@@ -52,30 +33,19 @@ func escape(_ width : Int, _ height : Int, _ time : inout Int, _ res : inout Str
         if building[curY][curX] == "*"
         {
             fire(width, height, curX, curY,prevTime, time, &visited, &building, &queue)
-            
-//            var _ = {
-//                for y in 0..<height
-//                    {
-//                    var test = ""
-//                    for x in 0..<width
-//                        {
-//                            test += building[y][x] + " "
-//                        }
-//                    print(test)
-//                }
-//            }()
         }
         for (dx, dy) in direction
         {
             let (nx,ny) = (curX + dx, curY + dy)
-            if isOutEdge(nx, ny, curX, curY, width, height, building: building, isEscape: &isEscape)
+            if nx < 0 || nx > width - 1 || ny < 0 || ny > height - 1
             {
+                if building[curY][curX] == "@"
+                {
+                    isEscape = true
+                    res += "\(time)\n"
+                    return
+                }
                 continue
-            }
-            if isEscape == true
-            {
-                res += "\(time)\n"
-                return
             }
             if building[ny][nx] == "." && !visited[ny][nx]
             {

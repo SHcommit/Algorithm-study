424275487fcc5390c71068cd92987cdac828f42c
ysh <happysh_s2@naver.com>
2022-07-01 21:50:53 +0900

    Create file

---
+++ Baekjoon/DynamicProgramming/[baekjoon]11055/[baekjoon]11055/main.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func BOJ_11055()
+{
+    let n    = Int(readLine()!)!
+    var list = readLine()!.split(separator: " ").map{Int(String($0))!}
+    var dp   = Array(repeating: 0, count: n)
+    for i in 0..<n
+    {
+        for j in 0...i
+        {
+            
+        }
+    }
+}
+BOJ_11055()

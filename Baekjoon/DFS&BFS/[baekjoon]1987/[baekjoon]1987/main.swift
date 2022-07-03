ddbf52ecdfd4427bda3dbd5f9386a66e242c44d9
ysh <happysh_s2@naver.com>
2022-07-03 21:55:59 +0900

    비트마스킹 공부좀 해봐야할듯

--- Baekjoon/DFS&BFS/[baekjoon]1987/[baekjoon]1987/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]1987/[baekjoon]1987/main.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+//비트마스킹과 bool 차이 공부해야함... ㅏ..
 typealias Element = (x : Int,y : Int, num : Int)
 let direction     = [(-1,0),(1,0),(0,1),(0,-1)]
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
@@ -12,12 +12,12 @@ for i in 0..<height
 {
     map[i] = readLine()!.map{Int(UnicodeScalar(String($0))!.value - Aascii)}
 }
-var visited = Array(repeating: false, count: 26)
-visited[map[0][0]] = true
-dfs((0,0,count))
+//var visited = Array(repeating: false, count: 26)
+//visited[map[0][0]] = true
+dfs((0,0,count), 1 << map[0][0])
 print(count)
 
-func dfs(_ element : Element)
+func dfs(_ element : Element, _ bitmask: Int)
 {
     count = max(count , element.num)
     for (dx,dy) in direction
@@ -25,13 +25,11 @@ func dfs(_ element : Element)
         let (nx,ny,curCount) = (element.x + dx, element.y + dy, element.num)
         if nx < 0 || nx > width - 1 || ny < 0 || ny > height - 1
         { continue }
-        let char = map[ny][nx]
-        if !visited[char]
+        //let char = map[ny][nx]
+        let nb = 1 << map[ny][nx]
+        if bitmask & nb == 0
         {
-            visited[char] = true
-            count = max(count, curCount + 1)
-            dfs((nx,ny,curCount + 1))
-            visited[char] = false
+            dfs((nx,ny,curCount + 1),bitmask | nb)
         }
     }
 }

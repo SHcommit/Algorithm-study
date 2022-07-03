13404a1b99fb25cc9903c655482b3430ddb353a8
ysh <happysh_s2@naver.com>
2022-07-03 21:41:41 +0900

    이거.. visit를 bool로하면 시간초과나서.. 비트마스킹?으로 해결해야한데;;ㅠ

--- Baekjoon/DFS&BFS/[baekjoon]1987/[baekjoon]1987/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]1987/[baekjoon]1987/main.swift
@@ -1,45 +1,38 @@
 import Foundation
-typealias Element = (Int,Int,Int)
-let direction = [(-1,0),(1,0),(0,1),(0,-1)]
 
-func dfs(_ element : Element, _ width : Int ,_ height : Int,_ count : inout Int, _ visited : inout [String], _ map : [[String]])
+typealias Element = (x : Int,y : Int, num : Int)
+let direction     = [(-1,0),(1,0),(0,1),(0,-1)]
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let height  = input[0]
+let width   = input[1]
+let Aascii  = UnicodeScalar("A").value
+var count   = 1
+var map     = Array(repeating: [Int](), count: height)
+for i in 0..<height
 {
-    count = max(count , element.2)
+    map[i] = readLine()!.map{Int(UnicodeScalar(String($0))!.value - Aascii)}
+}
+var visited = Array(repeating: false, count: 26)
+visited[map[0][0]] = true
+dfs((0,0,count))
+print(count)
+
+func dfs(_ element : Element)
+{
+    count = max(count , element.num)
     for (dx,dy) in direction
     {
-        let (nx,ny,curCount) = (element.0 + dx, element.1 + dy, element.2)
+        let (nx,ny,curCount) = (element.x + dx, element.y + dy, element.num)
         if nx < 0 || nx > width - 1 || ny < 0 || ny > height - 1
         { continue }
-        var visit = false
-        for str in visited
+        let char = map[ny][nx]
+        if !visited[char]
         {
-            if str == map[ny][nx]
-            {
-                visit = true
-                break
-            }
-        }
-        if !visit
-        {
-            visited.append(map[ny][nx])
-            dfs((nx,ny,curCount + 1), width, height, &count, &visited, map)
-            visited.removeLast()
+            visited[char] = true
+            count = max(count, curCount + 1)
+            dfs((nx,ny,curCount + 1))
+            visited[char] = false
         }
     }
 }
-func BOJ_1987()
-{
-    let input   = readLine()!.split(separator: " ").map{Int(String($0))!}
-    let width   = input[1]
-    let height  = input[0]
-    var count   = 1
-    var map     = Array(repeating: [String](), count: height)
-    for i in 0..<height
-    {
-        map[i] = readLine()!.map{String($0)}
-    }
-    var visited = [map[0][0]]
-    dfs((0,0,count), width, height, &count, &visited, map)
-    print(count)
-}
-BOJ_1987()
+

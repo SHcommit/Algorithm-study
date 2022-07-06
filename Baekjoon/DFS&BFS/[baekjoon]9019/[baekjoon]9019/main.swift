f34490d04b5e905129a97e7a2c9ef420311c0d3f
ysh <happysh_s2@naver.com>
2022-07-06 16:48:07 +0900

    Refactor code

--- Baekjoon/DFS&BFS/[baekjoon]9019/[baekjoon]9019/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]9019/[baekjoon]9019/main.swift
@@ -1,107 +1,58 @@
 import Foundation
-typealias Element = (curNum : Int, command : String)
-
 class Q
 {
-    var queue : [Element]
+    var queue : [(Int,String)]
     var index : Int
     init()
     {
-        queue = [Element]()
+        queue = [(Int,String)]()
         index = 0
     }
 }
-class calculator
+func D (_ number : Int) -> Int
 {
-    func D (_ number : Int) -> Int
+    return (number*2) % 10000
+}
+func S (_ number : Int) -> Int
+{
+    if number == 0
     {
-        return (number*2) % 10000
+        return 9999
     }
-    func S (_ number : Int) -> Int
-    {
-        if number == 0
-        {
-            return 9999
-        }
-        return number - 1
-    }
-    func L (_ number : Int) -> Int
-    {
-        return (number % 1000) * 10 + number/1000
-    }
-    func R (_ number : Int) -> Int
-    {
-        return (number % 10) * 1000 + number/10
-    }
+    return number - 1
+}
+func L (_ number : Int) -> Int
+{
+    return (number % 1000) * 10 + number/1000
+}
+func R (_ number : Int) -> Int
+{
+    return (number % 10) * 1000 + number/10
 }
 func BFS(_ start : Int, _ end : Int, res : inout String)
 {
-    let calculator = calculator()
-    var visited    = Array(repeating: false, count: 10000)
-    let q          = Q()
+    var visited = Array(repeating: false, count: 10000)
+    let q       = Q()
     
     q.queue.append((start,""))
-    /**
-     * 여기서 if curNumber == end {} 를 하고 아래의 if nNumver == end{} 를 사용하지 않으면 시간이 더 걸린다.
-     */
     while q.queue.count != q.index
     {
-        let (curNumber, command) = q.queue[q.index]
-        visited[curNumber] = true
+        let (curNum, command) = q.queue[q.index]
+        visited[curNum] = true
         q.index += 1
         
-        
-//        if curNumber == end
-//        {
-//            res += command + "\n"
-//            return
-//        }
-        var nNumber = calculator.D(curNumber)
-        if nNumber == end
+        for (nNum,nCommand) in [(D(curNum),"D"),(S(curNum),"S"),(L(curNum),"L"),(R(curNum),"R")]
         {
-            res += command + "D\n"
-            return
-        }
-        if !visited[nNumber]
-        {
-            q.queue.append((nNumber, command + "D"))
-            visited[nNumber] = true
-        }
-        
-        nNumber = calculator.S(curNumber)
-        if nNumber == end
-        {
-            res += command + "S\n"
-            return
-        }
-        if !visited[nNumber]
-        {
-            q.queue.append((nNumber, command + "S"))
-            visited[nNumber] = true
-        }
-        
-        nNumber = calculator.L(curNumber)
-        if nNumber == end
-        {
-            res += command + "L\n"
-            return
-        }
-        if !visited[nNumber]
-        {
-            q.queue.append((nNumber, command + "L"))
-            visited[nNumber] = true
-        }
-        
-        nNumber = calculator.R(curNumber)
-        if nNumber == end
-        {
-            res += command + "R\n"
-            return
-        }
-        if !visited[nNumber]
-        {
-            q.queue.append((nNumber, command + "R"))
-            visited[nNumber] = true
+            if nNum == end
+            {
+                res += command + nCommand + "\n"
+                return
+            }
+             if !visited[nNum]
+            {
+                 q.queue.append((nNum,command + nCommand))
+                 visited[nNum] = true
+             }
         }
     }
 }
@@ -119,6 +70,3 @@ func BOJ_9019()
     print(res)
 }
 BOJ_9019()
-
-
-

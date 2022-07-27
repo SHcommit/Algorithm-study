71d096b45c4d89d5590d7cdf9175a428c712c296
ysh <happysh_s2@naver.com>
2022-07-27 22:49:56 +0900

    BOJ_12738.swift

---
+++ Baekjoon/LongestIncreasingSubsequence/[baekjoon]12738/[baekjoon]12738/main.swift
@@ -0,0 +1,38 @@
+import Foundation
+func binarySearch(_ target : Int, _ seq : inout [Int])
+{
+    var left = 0 , right = seq.count - 1
+    while left > right
+    {
+        let mid = (left + right) / 2
+        if seq[mid] < target
+        {
+            left = mid + 1
+        }
+        else
+        {
+            right = mid
+        }
+    }
+    seq[right] = target
+}
+func BOJ_12738()
+{
+    let n = Int(readLine()!)!
+    let list = readLine()!.split(separator:" ").map{Int(String($0))!}
+    var seq = [list[0]]
+    
+    for i in 1..<n
+    {
+        if list[i] > seq.last!
+        {
+            seq.append(list[i])
+        }
+        else
+        {
+            binarySearch(list[i], &seq)
+        }
+    }
+    print(seq.count)
+}
+BOJ_12738()

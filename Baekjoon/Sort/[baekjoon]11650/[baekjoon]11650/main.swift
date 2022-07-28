56dbf7a98621e163375da6b89538be19c26cc694
ysh <happysh_s2@naver.com>
2022-07-28 22:47:42 +0900

    Solve the problem. BOJ_11650.swift

--- Baekjoon/Sort/[baekjoon]11650/[baekjoon]11650/main.swift
+++ Baekjoon/Sort/[baekjoon]11650/[baekjoon]11650/main.swift
@@ -1,11 +1,33 @@
-//
-//  main.swift
-//  [baekjoon]11650
-//
-//  Created by 양승현 on 2022/07/28.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+func BOJ_11650()
+{
+    let n = Int(readLine()!)!
+    var list : [(x:Int,y:Int)] = [(Int,Int)]()
+    for _ in 0..<n
+    {
+        let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+        list.append((input[0],input[1]))
+    }
+    list.sort
+    {
+        if $0.x<$1.x
+        {
+            return true
+        }
+        else if $0.x == $1.x
+        {
+            if $0.y<$1.y
+            {
+                return true
+            }
+            return false
+        }
+        return false
+    }
+    for (x,y) in list
+    {
+        print("\(x) \(y)")
+    }
+}
+BOJ_11650()

af94f1d76324d07d48418026622a9654bd1423d3
ysh <happysh_s2@naver.com>
2022-08-06 23:09:48 +0900

    Solve the problem. BOJ_11048.swift

--- Baekjoon/DynamicProgramming/[baekjoon]11048/[baekjoon]11048/main.swift
+++ Baekjoon/DynamicProgramming/[baekjoon]11048/[baekjoon]11048/main.swift
@@ -1,11 +1,22 @@
-//
-//  main.swift
-//  [baekjoon]11048
-//
-//  Created by 양승현 on 2022/08/06.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+func BOJ_11048()
+{
+    let hw = readLine()!.split(separator:" ").map{Int(String($0))!}
+    let (height,width) = (hw[0],hw[1])
+    var rooms = Array(repeating:[Int](), count: height)
+    for i in 0..<height
+    {
+        rooms[i] = readLine()!.split(separator:" ").map{Int(String($0))!}
+    }
+    var dp = Array(repeating: Array(repeating: 0, count: width+1), count: height+1)
+    for y in 1...height
+    {
+        for x in 1...width
+        {
+            dp[y][x] = max(dp[y-1][x-1] ,dp[y-1][x], dp[y][x-1]) + rooms[y-1][x-1]
+        }
+    }
+    print(dp[height][width])
+}
+BOJ_11048()

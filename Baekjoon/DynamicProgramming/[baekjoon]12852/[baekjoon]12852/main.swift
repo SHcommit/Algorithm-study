647320da1ba0e7aaa56fe8f78aa3b56c85067f95
ysh <happysh_s2@naver.com>
2022-08-07 23:25:17 +0900

    Solve the problem. BOJ_12852.swift

--- Baekjoon/DynamicProgramming/[baekjoon]12852/[baekjoon]12852/main.swift
+++ Baekjoon/DynamicProgramming/[baekjoon]12852/[baekjoon]12852/main.swift
@@ -1,11 +1,46 @@
-//
-//  main.swift
-//  [baekjoon]12852
-//
-//  Created by 양승현 on 2022/08/07.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+func bfs(_ n: Int)
+{
+    var queue = [(n,"\(n) ",0)]
+    var visited = Array(repeating: false , count : n+1)
+    visited[n] = true
+    var index = 0
+    var curCnt = 0
+    while index != queue.count
+    {
+        let (num,ans,count) = queue[index]
+        if num == 1
+        {
+            print(count)
+            print(ans)
+            return
+        }
+        if curCnt == count
+        {
+            curCnt += 1
+        }
+        index += 1
+        if num % 3 == 0 && num / 3 >= 1 && !visited[num/3]
+        {
+            visited[num/3] = true
+            queue.append((num/3,ans+"\(num/3) ",count+1))
+        }
+        if num % 2 == 0 && num / 2 >= 1 && !visited[num/2]
+        {
+            visited[num/2] = true
+            queue.append((num/2,ans+"\(num/2) ",count+1))
+        }
+        if num-1 >= 1 && !visited[num-1]
+        {
+            visited[num-1] = true
+            queue.append((num-1,ans+"\(num-1) ",count+1))
+        }
+    }
+}
+func BOJ_12852()
+{
+    let n = Int(readLine()!)!
+    bfs(n)
+}
+BOJ_12852()

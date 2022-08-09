634790f2b9603f664bed313fc4cbe1076a6ffee9
ysh <happysh_s2@naver.com>
2022-08-09 16:44:25 +0900

    Solve the problem.

--- Baekjoon/DynamicProgramming/[baekjoon]2631/[baekjoon]2631/main.swift
+++ Baekjoon/DynamicProgramming/[baekjoon]2631/[baekjoon]2631/main.swift
@@ -1,11 +1,62 @@
-//
-//  main.swift
-//  [baekjoon]2631
-//
-//  Created by 양승현 on 2022/08/09.
-//
-
 import Foundation
-
-print("Hello, World!")
-
+//func binarySearch(_ target: Int, _ seq : inout [Int])
+//{
+//    var left = 0, right = seq.count - 1
+//    while left < right
+//    {
+//        let mid = (left+right)/2
+//        if seq[mid] < target
+//        {
+//            left = mid + 1
+//        }
+//        else
+//        {
+//            right = mid
+//        }
+//    }
+//    seq[right] = target
+//}
+func BOJ_2631()
+{
+    let n    = Int(readLine()!)!
+    var list = [Int]()
+    for _ in 0..<n
+    {
+        list.append(Int(readLine()!)!)
+    }
+    var seq  = [list[0]]
+    for i in 1..<n
+    {
+        if seq.last! < list[i]
+        {
+            seq.append(list[i])
+        }
+        else
+        {
+            binarySearch(list[i], &seq)
+        }
+    }
+    print(n - seq.count)
+//    var dp = Array(repeating: 0, count: n)
+//    for i in 0..<n
+//    {
+//        dp[i] = 1
+//        for j in 0..<i
+//        {
+//            if list[j] < list[i] && dp[j]+1 > dp[i]
+//            {
+//                dp[i] = dp[j] + 1
+//            }
+//        }
+//    }
+//    var ans = 0
+//    for i in 0..<n
+//    {
+//        if ans < dp[i]
+//        {
+//            ans = dp[i]
+//        }
+//    }
+//    print(ans)
+}
+BOJ_2631()

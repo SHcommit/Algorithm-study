3b4ff9829343eaa1f927481af4de1a95e44fe95e
ysh <happysh_s2@naver.com>
2022-08-09 14:11:35 +0900

    Create file

---
+++ Baekjoon/DynamicProgramming/[baekjoon]2631/[baekjoon]2631/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  [baekjoon]2631
+//
+//  Created by 양승현 on 2022/08/09.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- Baekjoon/TwoPointer/[baekjoon]3273/[baekjoon]3273/main.swift
+++ Baekjoon/TwoPointer/[baekjoon]3273/[baekjoon]3273/main.swift
@@ -5,18 +5,21 @@ func BOJ_3273()
     let n    = Int(readLine()!)!
     let list = readLine()!.split(separator:" ").map{Int(String($0))!}.sorted(by: <)
     let x    = Int(readLine()!)!
-    var sum  = 0 ,cnt = 0
-    for left in 0..<n
+    var sum  = 0 ,cnt = 0, left = 0, right = list.count - 1 
+    while left < right
     {
-        var right = left + 1
-        while sum<x && right < n
+        sum = list[left]+list[right]
+        if sum >= x
         {
-            sum = list[left] + list[right]
-            right += 1
+            if sum == x
+            {
+                cnt += 1
+            }
+            right -= 1
         }
-        if sum == x
+        else
         {
-            cnt += 1
+            left += 1
         }
     }
     print(cnt)

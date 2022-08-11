56b892e6b30cfe7fa7b1ecd022dd41f524bbb73f
ysh <happysh_s2@naver.com>
2022-08-12 03:08:27 +0900

    에러남. 왜그럴까

---
+++ Baekjoon/TwoPointer/[baekjoon]11728/[baekjoon]11728/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  [baekjoon]11728
+//
+//  Created by 양승현 on 2022/08/12.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- Baekjoon/TwoPointer/[baekjoon]17609/[baekjoon]17609/main.swift
+++ Baekjoon/TwoPointer/[baekjoon]17609/[baekjoon]17609/main.swift
@@ -1,11 +1,61 @@
-//
-//  main.swift
-//  [baekjoon]17609
-//
-//  Created by 양승현 on 2022/08/11.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+func BOJ_17609()
+{
+    let n = Int(readLine()!)!
+    var ans = ""
+    for _ in 0..<n
+    {
+        let str = readLine()!.map{String($0)}
+        var left = 0, right = str.count - 1 ,check = 0
+        while left < right
+        {
+            if str[left] == str[right]
+            {
+                left  += 1
+                right -= 1
+            }
+            else
+            {
+                var newLeft  = left + 1, newRight = right
+                while(newLeft<newRight)
+                {
+                    if str[newLeft] != str[newRight]
+                    {
+                        check += 1
+                        break
+                    }
+                    newLeft  += 1
+                    newRight -= 1
+                }
+                newLeft  = left
+                newRight = right - 1
+                while(newLeft<=newRight)
+                {
+                    if str[newLeft] != str[newRight]
+                    {
+                        check += 1
+                        break
+                    }
+                    newLeft += 1
+                    newRight -= 1
+                }
+                if check == 0
+                {
+                    check += 1
+                }
+                break
+            }
+        }
+        if ans == ""
+        {
+            ans += "\(check)"
+        }
+        else
+        {
+            ans += "\n\(check)"
+        }
+    }
+    print(ans)
+}
+BOJ_17609()

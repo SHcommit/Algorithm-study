0e38a00131fbcc19629abe2d94b14652fcf53dfc
ysh <happysh_s2@naver.com>
2022-08-14 01:16:39 +0900

    이문제 별찍기인데 별찍기 뒤에 공백을 넣으면 안됨ㅋㅋ...

--- Baekjoon/Normal/[baekjoon]2442/[baekjoon]2442/main.swift
+++ Baekjoon/Normal/[baekjoon]2442/[baekjoon]2442/main.swift
@@ -1,11 +1,42 @@
-//
-//  main.swift
-//  [baekjoon]2442
-//
-//  Created by 양승현 on 2022/08/14.
-//
-
 import Foundation
 
-print("Hello, World!")
+func BOJ_2442()
+{
+    let n    = Int(readLine()!)!
+    var star = Array(repeating: " ", count: n*2 - 1)
+    var left = n-1, right = n-1
+    for _ in 0..<n
+    {
+        if left == right
+        {
+            star[left] = "*"
+            left  -= 1
+            right += 1
+        }
+        else
+        {
+            star[left]  = "*"
+            star[right] = "*"
+            left  -= 1
+            right += 1
+        }
+        var flag = false
+        var ans = ""
+        for i in 0..<(n*2 - 1)
+        {
+            if flag && star[i] != "*"
+            {
+                break
+            }
+            if star[i] == "*"
+            {
+                flag = true
+            }
+            ans += star[i]
+            
+        }
+        print(ans)
+    }
+}
+BOJ_2442()
 

3264c90fa45f66450a99751e67883788cac7905f
ysh <happysh_s2@naver.com>
2023-01-18 22:09:56 +0900

    Create

---
+++ Baekjoon/Dijkstra/[baekjoon]14284/[baekjoon]14284/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  [baekjoon]14284
+//
+//  Created by 양승현 on 2023/01/18.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Baekjoon/String/[baekjoon]13417/[baekjoon]13417/main.swift
@@ -0,0 +1,6 @@
+//
+//  main.swift
+//  [baekjoon]13417
+//
+//  Created by 양승현 on 2023/01/18.
+//

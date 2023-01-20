5a5d1f061868d3eb2d8fa3710366d9154d817fa3
ysh <happysh_s2@naver.com>
2023-01-20 23:44:37 +0900

    10808.swift

---
+++ Baekjoon/Normal/[baekjoon]10808/[baekjoon]10808/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  [baekjoon]10808
+//
+//  Created by 양승현 on 2023/01/20.
+//
+
+import Foundation
+
+func start(){
+    var arr = Array(repeating: 0, count: 26)
+    _=readLine()!.map{Character(String($0))}.map { ch in
+        arr[Int(ch.asciiValue!)-97] += 1
+    }
+    print(arr.map{String($0)}.joined(separator: " "))
+}
+start()

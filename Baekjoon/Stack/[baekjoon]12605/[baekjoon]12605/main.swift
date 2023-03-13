8ad54a3592fc8bbb0b2bd706a8d02bc696b8fc89
ysh <happysh_s2@naver.com>
2023-03-13 23:59:32 +0900

    12605.swift

--- Baekjoon/Stack/[baekjoon]12605/[baekjoon]12605/main.swift
+++ Baekjoon/Stack/[baekjoon]12605/[baekjoon]12605/main.swift
@@ -10,3 +10,38 @@ import Foundation
 (1...Int(readLine()!)!).map{
     print("Case #\($0): \(readLine()!.split{$0==" "}.reversed().joined(separator: " "))")
 }
+
+
+struct Stack {
+    private var lists: [String]
+    init(lists: [String] = [String]()) {
+        self.lists = lists
+    }
+}
+extension Stack {
+    mutating func push(_ element: String) {
+        lists.append(element)
+    }
+    mutating func pop() -> String?{
+        guard count() != 0 else { return nil }
+        return lists.removeLast()
+    }
+    func count() -> Int {
+        return lists.count
+    }
+    func top() -> String {
+        return lists[count()-1]
+    }
+}
+
+_=(1...Int(readLine()!)!).map{ caseIndex in
+    var stack = Stack()
+    var reversedText = ""
+    for text in readLine()!.split{$0==" "} {
+        stack.push(String(text))
+    }
+    while let text = stack.pop() {
+        reversedText += "\(text) "
+    }
+    print("Case #\(caseIndex): \(reversedText)")
+}

82b018ec8519112973d4f4492da87cd047c51afa
ysh <happysh_s2@naver.com>
2023-03-31 23:43:36 +0900

    14405.swift

---
+++ Baekjoon/String/[baekjoon]14405/[baekjoon]14405/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  [baekjoon]14405
+//
+//  Created by 양승현 on 2023/03/31.
+//
+
+import Foundation
+print(readLine()!.range(of:"^(pi|ka|chu)*$",options: .regularExpression)==nil ? "NO":"YES")

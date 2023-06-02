4eb468b62b9fa7f8a958c61d91d62890a9633694
ysh <happysh_s2@naver.com>
2023-06-03 01:05:48 +0900

    9205.swift

--- Baekjoon/DFS&BFS/[baekjoon]9205/[baekjoon]9205/main.swift
+++ Baekjoon/DFS&BFS/[baekjoon]9205/[baekjoon]9205/main.swift
@@ -1,71 +1,35 @@
 import Foundation
-typealias point = (x:Int,y:Int)
-let direction = [(0,1000),(0,-1000),(1000,0),(-1000,0)]
-func bfs(_ start : point, _ end : point, _ storeList : [point],_ res : inout String)
-{
-    var queue    = [(start.x,start.y)]
-    var index    = 0
-    var visitedX = Set<Int>()
-    var visitedY = Set<Int>()
-    visitedX.insert(start.x)
-    visitedY.insert(start.y)
-    while queue.count != index
-    {
-        let (curX,curY) = queue[index]
-        index += 1
-        for (dx,dy) in direction
-        {
-            let (nx,ny) = (curX+dx,curY+dy)
-            if nx >= end.x && ny >= end.y
-            {
-                res += "happy\n"
-                return
-            }
-            if nx > 32767 || nx < -32768 || ny > 32767 || ny < -32768
-            {
-                continue
-            }
-            var isStore = false
-            for (storeX,storeY) in storeList
-            {
-                if storeX == nx && storeY == ny
-                {
-                    isStore = true
-                    break
-                }
-            }
-            if isStore
-            {
-                let (isVisitedX,_) = visitedX.insert(nx)
-                let (isVisitedY,_) = visitedY.insert(ny)
-                if !isVisitedX || !isVisitedY
-                {
-                    queue.append((nx,ny))
-                }
-            }
-        }
-    }
-    res += "sad\n"
+
+typealias Point = (x: Int, y: Int)
+let testCase = Int(readLine()!)!
+
+_=(0..<testCase).map { _ in
+  print(isAnswer() ? "happy" : "sad")
 }
-func BOJ_9205()
-{
-    let n   = Int(readLine()!)!
-    var res = ""
-    for _ in 0..<n
-    {
-        let store = Int(readLine()!)!
-        let startPoint = readLine()!.split(separator:" ").map{Int(String($0))!}
-        let (startX,startY) = (startPoint[0],startPoint[1])
-        var storeList = [(Int,Int)]()
-        for _ in 0..<store
-        {
-            let input = readLine()!.split(separator:" ").map{Int(String($0))!}
-            storeList.append((input[0],input[1]))
-        }
-        let endPoint = readLine()!.split(separator:" ").map{Int(String($0))!}
-        let (endX,endY) = (endPoint[0],endPoint[1])
-        bfs((startX,startY), (endX,endY), storeList, &res)
+func isAnswer() -> Bool {
+  let n = Int(readLine()!)!
+  let inputStart = readLine()!.split{$0==" "}.map { Int(String($0))! }
+  let inputStore = (0..<n).map {_ in return readLine()!.split{$0==" "}.map{Int(String($0))!} }
+  let inputEnd = readLine()!.split{$0==" "}.map { Int(String($0))!}
+  let start = (inputStart[0], inputStart[1])
+  let stores: [Point] = (0..<n).map { (inputStore[$0][0], inputStore[$0][1]) }
+  var visited = Array(repeating: false, count: 100)
+  let end: Point = (inputEnd[0], inputEnd[1])
+  var queue: [Point] = [start]
+  var idx = 0
+  while queue.count != idx {
+    var cp = queue[idx]
+    if abs(cp.x - end.x) + abs(cp.y - end.y) <= 1000 {
+      return true
     }
-    print(res)
+    for i in 0..<n where !visited[i] {
+      let d = abs(cp.x - stores[i].x) + abs(cp.y - stores[i].y)
+      if d <= 1000 {
+        visited[i] = true
+        queue.append((stores[i].x, stores[i].y))
+      }
+    }
+    idx += 1
+  }
+  return false
 }
-BOJ_9205()

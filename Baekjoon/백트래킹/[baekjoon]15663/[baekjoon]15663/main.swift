4f52378b025b5a3e56bc99380e517a0cbc3f2121
shcommit <happysh_s2@naver.com>
2024-01-12 23:10:18 +0900

    15663.swift 풀음 근데 왜  NSOrderedSet 써ㄹ서 한거는 왜 안되는거지? 출력초과라하는데;,ㅠ

---
+++ Baekjoon/백트래킹/[baekjoon]15663/[baekjoon]15663/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  [baekjoon]15663
+//
+//  Created by 양승현 on 1/11/24.
+//
+
+import Foundation
+let nm = readLine()!.split{$0==" "}.map{Int($0)!}
+let s = readLine()!.split{$0==" "}.map{Int($0)!}.sorted()
+var res: [String] = []
+var isUsed = Array(repeating: false, count: nm[0])
+var seq = Array(repeating: 0, count: nm[1])
+var duplicated = Set<String>()
+func sol(_ k: Int) {
+  if k == nm[1] {
+    let text = seq.map { String($0) }.joined(separator: " ")
+    if duplicated.contains(text) { return }
+    duplicated.insert(text)
+    res.append(text)
+    return
+  }
+  for i in 0..<nm[0] where !isUsed[i] {
+    isUsed[i].toggle()
+    seq[k] = s[i]
+    sol(k+1)
+    isUsed[i].toggle()
+  }
+}
+sol(0)
+print(res.joined(separator: "\n"))
+//(NSOrderedSet(array: res).array as! [String]).forEach{print($0)}

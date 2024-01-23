41c8d93210269da7bee20eca7f1294089a769b18
shcommit <happysh_s2@naver.com>
2024-01-24 01:28:04 +0900

    16987.swift

--- Baekjoon/백트래킹/[baekjoon]16987/[baekjoon]16987/main.swift
+++ Baekjoon/백트래킹/[baekjoon]16987/[baekjoon]16987/main.swift
@@ -8,20 +8,12 @@
 import Foundation
 
 /*
- 각 계란에는 내구도-무게 정해짐.
- 계란으로 계란치면 각 계란의 내구도는 상대 계란의 무게만큼 깎이게 됨. 내구도 0 이하 되는 순 간 계란은 깨지게 됨.
+ 우선 아쉬운것은.. 문제를 잘못이해했다는 것임ㅠㅅㅠ
+ 문제에서 가장 최근에 든 계란이 가장 오른쪽에 위치한 계란일 경우 계란 치는 과정을 종료한다고했는데
+ 이게 가장 오른쪽 계란은 그냥 치지 않고 종료하는거로 생각을 했거든..
  
- ex) 계1의 내구도 7, 무 5.
-     계2의 내구도 3, 무 4.
- 
-
- 계란 1로 계란 2치면 계란 1의 내구도는 4 감소함. 계란2의 내구도는 5만큼 감소함.
- 즉 서로의 무게만큼 내구도가 까임.
- 충돌 결과. 계2는 깨졌고, 계란1은 내구도 3으로 남아있음.
- 
- 왼쪽부터 차례로 들어서 한번씩만 다른 계란을 쳐 최대한 많은 계란 을 깨는 문제임.
- 1. 가장 왼쪽 계란 듬.
- 2. 손에든 계란은 제자리에 두고, 다른 계란듬.
+ 그리고 이 문제를 풀면서 여러번 틀렸는데 그 이유는 만약에 특정 시점에 계란의 def는 높은데, 다른 계란들이 이미 다 깨졌을 경우야.
+ 이때 더이상 k는 증가되지 않고 그 시점에서의 깨진계란이 몇개인지 세야하는데 그냥 종료되버려. 이게 문제였으.
  */
 
 typealias E = (def: Int, att: Int)
@@ -33,18 +25,24 @@ var eggs: [E] = (0..<n).map {_ in
 var res = 0
 func recur(of k: Int) {
   if k == n {
-    res = max(res,eggs.filter {$0.def <= 0}.count)
+    res = max(res,eggs.filter{$0.def <= 0}.count)
     return
   }
-  if eggs[k].def <= 0 { recur(of: k+1) }
-  for j in 0..<n where k != j && eggs[j].def > 0 {
+  if eggs[k].def <= 0 {
+    recur(of: k+1)
+    return
+  }
+  var f = false
+  for j in 0..<n where k != j && eggs[j].def>0 {
     eggs[j].def -= eggs[k].att
     eggs[k].def -= eggs[j].att
+    f = true
     recur(of: k+1)
     eggs[j].def += eggs[k].att
     eggs[k].def += eggs[j].att
   }
+  ///만약 체력이 높지만 다른거들 안 내리칠때는 그 때의 계란이 몇개 깨졌는지도 구해주어야함.
+  if !f { recur(of: n) }
 }
-
 recur(of: 0)
 print(res)

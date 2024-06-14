3d607e1674e19cdfd94c7bb7b30aa0f44c20036a
shcommit <happysh_s2@naver.com>
2024-06-14 13:33:19 +0900

    호텔 대실.swift 간소화

--- Programmers/Level2/호텔 대실/호텔 대실/main.swift
+++ Programmers/Level2/호텔 대실/호텔 대실/main.swift
@@ -7,6 +7,53 @@
 
 import Foundation
 
+//typealias Element = (s: Int, e: Int)
+//func solution(_ book_time:[[String]]) -> Int {
+//  var currentUsingTime: [Element] = []
+//  var mostUsedRoomInSpecTime: Int = 0
+//  let booking: [Element] = book_time.map {
+//    let s = toMin(from: $0[0])
+//    let e = toMin(from: $0[1])
+//    return (s,e+10)
+//  }.sorted { $0.s < $1.s }
+//  
+//  booking.forEach {
+//    if currentUsingTime.isEmpty {
+//      currentUsingTime.append($0)
+//      mostUsedRoomInSpecTime = max(currentUsingTime.count, mostUsedRoomInSpecTime)
+//    } else {
+//      var idx = 0
+//      var isNeededNewRoom = false
+//      while idx < currentUsingTime.count {
+//        let usingTime = currentUsingTime[idx]
+//        if usingTime.s >= $0.s || usingTime.e > $0.s {
+//          isNeededNewRoom = true
+//        } else if usingTime.e <= $0.s {
+//          currentUsingTime.remove(at: idx)
+//          currentUsingTime.append($0)
+//          isNeededNewRoom = false
+//          break
+//        }
+//        idx += 1
+//      }
+//      if isNeededNewRoom {
+//        currentUsingTime.append($0)
+//        mostUsedRoomInSpecTime = max(currentUsingTime.count, mostUsedRoomInSpecTime)
+//      }
+//    }
+//  }
+//  
+//  return mostUsedRoomInSpecTime
+//}
+//
+//@inline(__always)
+//func toMin(from h_m: String) -> Int {
+//  let hm = h_m.split{$0==":"}.map {Int($0)!}
+//  return hm[0]*60 + hm[1]
+//}
+
+
+// MARK: - 최대한 빨리 푸는데 초점을 맞췄고 중복 로직을 추리면..
 typealias Element = (s: Int, e: Int)
 func solution(_ book_time:[[String]]) -> Int {
   var currentUsingTime: [Element] = []
@@ -18,31 +65,23 @@ func solution(_ book_time:[[String]]) -> Int {
   }.sorted { $0.s < $1.s }
   
   booking.forEach {
-    if currentUsingTime.isEmpty {
+    var idx = 0
+    var isNeededNewRoom = true
+    while idx < currentUsingTime.count {
+      let usingTime = currentUsingTime[idx]
+      if usingTime.e <= $0.s {
+        currentUsingTime.remove(at: idx)
+        currentUsingTime.append($0)
+        isNeededNewRoom = false
+        break
+      }
+      idx += 1
+    }
+    if isNeededNewRoom {
       currentUsingTime.append($0)
       mostUsedRoomInSpecTime = max(currentUsingTime.count, mostUsedRoomInSpecTime)
-    } else {
-      var idx = 0
-      var isNeededNewRoom = false
-      while idx < currentUsingTime.count {
-        let usingTime = currentUsingTime[idx]
-        if usingTime.s >= $0.s || usingTime.e > $0.s {
-          isNeededNewRoom = true
-        } else if usingTime.e <= $0.s {
-          currentUsingTime.remove(at: idx)
-          currentUsingTime.append($0)
-          isNeededNewRoom = false
-          break
-        }
-        idx += 1
-      }
-      if isNeededNewRoom {
-        currentUsingTime.append($0)
-        mostUsedRoomInSpecTime = max(currentUsingTime.count, mostUsedRoomInSpecTime)
-      }
     }
   }
-  
   return mostUsedRoomInSpecTime
 }
 
@@ -51,3 +90,4 @@ func toMin(from h_m: String) -> Int {
   let hm = h_m.split{$0==":"}.map {Int($0)!}
   return hm[0]*60 + hm[1]
 }
+

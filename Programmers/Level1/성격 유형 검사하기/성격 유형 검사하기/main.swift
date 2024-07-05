aeee07d0c80e142c892194f72670079992f73db8
shcommit <happysh_s2@naver.com>
2024-07-05 13:09:17 +0900

    성격 유형 검사하기.swift

---
+++ Programmers/Level1/성격 유형 검사하기/성격 유형 검사하기/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  성격 유형 검사하기
+//
+//  Created by 양승현 on 7/5/24.
+//
+
+import Foundation
+
+/// 주의할점:
+/// 4보다 클 경우는 choice - 4를 해주는게 맞는데,
+/// 4보다 작을 경우엔 1번은 3점 이렇게 오름차순으로 시작되기에 4-choice를 해주어야함
+func solution(_ survey:[String], _ choices:[Int]) -> String {
+  let personality = "RTCFJMAN".map { String($0) }
+  var scores = personality.reduce(into: [String: Int]()) { $0[$1] = 0 }
+  for (i, input) in survey.enumerated() {
+    let s = input.map { String($0) }
+    let choice = choices[i]
+    if choice == 4 { continue }
+    if choice > 4 { scores[s[1]]! += choice - 4 }
+    else { scores[s[0]]! += 4 - choice }
+  }
+  return stride(from: 0, to: personality.count, by: 2).reduce("") {
+    let a = personality[$1]
+    let b = personality[$1+1]
+    if scores[a]! == scores[b]! { return $0 + (a < b ? a : b) }
+    return $0 + (scores[a]! > scores[b]! ? a : b)
+  }
+}

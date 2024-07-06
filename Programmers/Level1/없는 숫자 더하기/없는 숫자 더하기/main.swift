d96c893c5fa70a6cbaa66692d9bbab9c754899fa
shcommit <happysh_s2@naver.com>
2024-07-06 23:54:15 +0900

    aaaaa

--- Programmers/Level1/없는 숫자 더하기/없는 숫자 더하기/main.swift
+++ Programmers/Level1/없는 숫자 더하기/없는 숫자 더하기/main.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-func solution(_ numbers:[Int]) -> Int {
-  "0123456789".map{Int(String($0))!}.filter{!(numbers.contains($0))}.reduce(0,+)
+func solution(_ n:[Int]) -> Int {
+  (0...9).filter{!n.contains($0)}.reduce(0,+)
 }
 
 print(solution([5,8,4,0,6,7,9]))

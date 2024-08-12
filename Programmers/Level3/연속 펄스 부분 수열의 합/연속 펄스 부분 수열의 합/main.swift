cda6eee4b9d2dd8020c4c6f18b197865acc2f83c
shcommit <happysh_s2@naver.com>
2024-08-12 12:06:50 +0900

    연속 펄스 부분 수열의 합.swift

--- Programmers/Level3/연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합/main.swift
+++ Programmers/Level3/연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합/main.swift
@@ -7,10 +7,7 @@
 
 import Foundation
 
-/// 오 대박.. level3을 30분만에 풀어버렸다.
-///
-/// 슬라이딩 윈도우?같은 개념으로 풀어야하나.. 누적합?! 같은걸 생각하다가,, 일단 그냥 풀었는데
-/// 힌트를 보니 분할정복 방식의 부분수열 최대합을 구해보라고해서 이거를 한번 시도해봐야겠다.
+/// dp 부분 합 최대 구해감
 func solution(_ sequence:[Int]) -> Int64 {
   let length = sequence.count
   let plusBased = sequence.enumerated().map { $0%2 == 1 ? $1*(-1) : $1 }

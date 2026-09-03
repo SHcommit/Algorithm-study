561032ce0b88c52dbd78c33445c77d461a1cf8e3
shcommit <happysh_s2@naver.com>
2026-09-03 16:26:50 +0900

    가장 먼 노드.java

--- Programmers/Level3/가장_먼_노드.java
+++ Programmers/Level3/가장_먼_노드.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Solution {
     static List<Integer>[] graph;
     static int[] cache = new int[20001];
+    static int maxCost = 0;
     public int solution(int n, int[][] edges) {
         graph = new ArrayList[n+1];
         for(int i=0;i<=n;i++) graph[i] = new ArrayList<>();
@@ -12,28 +13,21 @@ class Solution {
         }
         int ans = 0;
         bfs();
-        int maxValue = 0;
-        for(int i=1;i<=n;i++) maxValue = Math.max(cache[i], maxValue);
-        for(int i=1;i<=n;i++) if(cache[i]==maxValue)ans++;
+        for(int i=1;i<=n;i++) if(cache[i]==maxCost)ans++;
         return ans;
     }
-    
-    class Edge {
-        int to, cost;
-        Edge(int t, int c) { to=t; cost=c;}
-    }
-    
     void bfs() {
-        Queue<Edge> queue = new ArrayDeque<>();
-        queue.add(new Edge(1,1));
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(1);
         cache[1]=1;
         while(!queue.isEmpty()) {
-            Edge cur = queue.poll();
-            for(int next: graph[cur.to]) {
-                int nextCost = cur.cost+1;
+            int cur = queue.poll();
+            for(int next: graph[cur]) {
                 if(cache[next]!=0) continue;
+                int nextCost = cache[cur]+1;
+                maxCost = Math.max(maxCost, nextCost);
                 cache[next] = nextCost;
-                queue.add(new Edge(next, nextCost));
+                queue.add(next);
             }
         }
     }
